9ef6c4dc4f09ff8750e991f6f2fe8ff4c9f3b684
Weber <101433196+domefox@users.noreply.github.com>
2023-08-04 01:39:25 -0400

    Update script.js

--- web/script.js
+++ web/script.js
@@ -1,1 +1,18 @@
+const flower = document.getElementById("flower");
+const balls = document.getElementById("balls");
+
+function bloom() {
+  flower.style.opacity = 1;
+  flower.style.transform = "scale(1)";
+}
+
+function rotate() {
+  for (let i = 0; i < balls.children.length; i++) {
+    const ball = balls.children[i];
+    ball.style.transform = `rotate(${(i * 360) / balls.children.length}deg)`
+  }
+}
+
+window.onload = bloom;
+window.setInterval(rotate, 1000);
 
